8daf86e718ef20685b7e2ff92655acbf550932b4
Vitsky <vitskyds@gmail.com>
2018-03-06 21:42:20 +0200

    refact: addd interactivity to timer

--- js/script.js
+++ js/script.js
@@ -12,36 +12,6 @@ let cardArray = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6],
         score: 1
     };
 
-const gameReset = function () {
-    // reset the board by randomizing the array
-    // cardArray.sort(function () {return 0.5 - Math.random()});
-    for (let i = 0; i < 12; i++) {
-        const newCard = document.createElement('div');
-        newCard.classList.add('card');
-        newCard.classList.add('card-' + cardArray[i]);
-        $(newCard).attr('id', i);
-        $(newCard).attr('data', 'card-' + cardArray[i]);
-        newCard.innerHTML = '<div class="side-a"></div><div class="side-b"></div>';
-        fragment.appendChild(newCard);
-    }
-    $('.cards-container').empty();
-    $('.cards-container').append(fragment); // reset timer
-    // reset rating and matches
-    matches = 0;
-    $('.rating').removeClass('rating-2-3 rating-1-3');
-    // hide all popups
-    $('.full-screen').fadeOut(0);
-
-}
-
-const updateGameStats = function () {
-    // update game stats
-};
-
-/*Initial game reset*/
-
-gameReset();
-
 /*Buttons*/
 
 $('.reset-button').on('click', function () {
@@ -63,7 +33,7 @@ $('.pull-down').on('click', function () {
 /*reset popup*/
 
 $('.reset-approve').on('click', function () {
-    reset();
+    gameReset();
 });
 
 $('.cancel-button').on('click', function () {
@@ -86,9 +56,99 @@ $('#player-name-input').on('update', function () {
     //update gameStats with new value for name
 });
 
+
+
+/*timer*/
+
+const stopwatch = function () {
+    const currentTime = document.getElementById('timer');
+    if (!currentTime) return;
+
+    const api = {};
+    const duration = 1000;
+    let time = 0;
+    let clocktimer;
+    let m, s;
+
+    function pad(num, size) {
+        let s = "0000" + String(num);
+        return s.substr(s.length - size);
+    }
+
+    function formatTime() {
+        time += duration;
+        m = Math.floor(time / (60 * 1000) % 60);
+        s = Math.floor(time / 1000 % 60);
+        return pad(m, 2) + ':' + pad(s, 2);
+    }
+
+    function update() {
+        currentTime.innerHTML = formatTime();
+    }
+
+    api.start = function () {
+        clocktimer = setInterval(update, duration);
+        api.running = true;
+    }
+
+    api.stop = function () {
+        clearInterval(clocktimer);
+        api.running = false;
+    }
+
+    api.formatTime = formatTime;
+
+    api.reset = function () {
+        time = 0;
+    }
+
+    return api
+}
+
+let timer = stopwatch();
+timer.stop();
+
+/*game reset*/
+
+const gameReset = function () {
+    // reset the board by randomizing the array
+    cardArray.sort(function () {
+        return 0.5 - Math.random()
+    });
+    for (let i = 0; i < 12; i++) {
+        const newCard = document.createElement('div');
+        newCard.classList.add('card');
+        newCard.classList.add('card-' + cardArray[i]);
+        $(newCard).attr('id', i);
+        $(newCard).attr('data', 'card-' + cardArray[i]);
+        newCard.innerHTML = '<div class="side-a"></div><div class="side-b"></div>';
+        fragment.appendChild(newCard);
+    }
+    $('.cards-container').empty();
+    $('.cards-container').append(fragment); // reset timer
+    // reset rating, matches and timer
+    matches = 0;
+    $('.rating').removeClass('rating-2-3 rating-1-3');
+    timer.stop();
+    timer.reset();
+    cardData = '';
+    cardId = 13;
+    $('#timer').text('00:00');
+    // hide all popups
+    $('.full-screen').fadeOut(0);
+
+}
+
+/*Initial game reset*/
+
+gameReset();
+
 /*card interaction*/
 
 $(document).on('click', '.card', function () {
+    if (timer.running === false) {
+        timer.start();
+    }
     // toggle .flipped class on card
     this.classList.toggle("flipped");
     // add 1 click to clicks
@@ -122,9 +182,10 @@ $(document).on('click', '.card', function () {
 
     //
     if (matches === 6) {
-
-        // stop timer
-        // update score
+        timer.stop()
+        gameStats.timer = timer.
+            // stop timer
+            // update score
         gameStats.score = gameStats.timer * gameStats.clicks;
         // display popup of success
         setTimeout(function () {
@@ -132,49 +193,3 @@ $(document).on('click', '.card', function () {
         }, 500);
     }
 });
-
-/*timer*/
-
-var stopwatch = function (my_element_id) {
-    var $time = document.getElementById('timer')
-    if (!$time) return
-
-    var api = {}
-    var duration = 1000
-    var time = 0
-    var clocktimer
-    var h, m, s, ms
-
-    function pad(num, size) {
-        var s = "0000" + String(num)
-        return s.substr(s.length - size)
-    }
-
-    function formatTime() {
-        time += duration
-        h = Math.floor(time / (60 * 60 * 1000))
-        m = Math.floor(time / (60 * 1000) % 60)
-        s = Math.floor(time / 1000 % 60)
-        ms = time % 1000 / 10
-        return pad(m, 2) + ':' + pad(s, 2)
-    }
-
-    function update() {
-        $time.innerHTML = formatTime()
-    }
-
-    api.start = function () {
-        clocktimer = setInterval(update, duration)
-    }
-
-    api.stop = function () {
-        clearInterval(clocktimer)
-    }
-
-    api.formatTime = formatTime
-
-    return api
-}
-
-var my_stopwatch = stopwatch('stopwatch');
-my_stopwatch.start();
