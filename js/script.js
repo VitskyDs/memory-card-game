c2be38f4aba6b8b02fa6bc5b4bcca106936500c9
Vitsky <vitskyds@gmail.com>
2018-03-10 10:33:58 +0200

    refac: refactored card evaluation logic

--- js/script.js
+++ js/script.js
@@ -3,14 +3,7 @@ let cardArray = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6],
     cardData = '',
     cardId = 13,
     score = 0,
-    fragment = document.createDocumentFragment(),
-    gameStats = {
-        playerName: '',
-        rating: 1,
-        timer: '',
-        clicks: 0,
-        score: 1
-    };
+    fragment = document.createDocumentFragment();
 
 const rating = document.getElementById('rating'),
     highscore = document.getElementById('highscore'),
@@ -19,25 +12,29 @@ const rating = document.getElementById('rating'),
     winner = document.getElementById('winner'),
     subMenu = document.getElementById('sub-menu'),
     cardsContainer = document.getElementById('cards-container'),
-    card = document.getElementsByClassName('card'),
     resetButton = document.getElementsByClassName('reset-button'),
+    gameStats = {
+        playerName: '',
+        rating: 1,
+        timer: '',
+        clicks: 0,
+        score: 1
+    },
     clock = $('#timer').timer({
         format: '%M:%S'
     });
 
-/*Functions*/
-
+//random function
 function random(min, max) {
     return Math.floor(Math.random() * (1 + max - min) + min);
 }
 
+//particle function
 function particleAnimation(particleNum) {
     const tlexplosion = new TimelineMax(),
         container = $("#particle-container"),
         dotsCount = Number(Math.round(particleNum / 10));
     let html = "";
-
-    console.log(dotsCount);
 
     for (let i = 0; i < dotsCount; i++) {
         html += "<div class='particle'></div>";
@@ -45,7 +42,7 @@ function particleAnimation(particleNum) {
 
     const dots = $(html).appendTo(container);
 
-    // animation
+    //animation
     dots.each(function () {
         tlexplosion.add(TweenMax.fromTo(this, 1.5, {
             z: random(-700, 700),
@@ -62,8 +59,7 @@ function particleAnimation(particleNum) {
     });
 }
 
-/*Buttons*/
-
+//Buttons
 $(resetButton).on('click', function () {
     $('.reset').fadeIn(0);
     subMenu.classList.remove('sub-menu-open');
@@ -78,8 +74,7 @@ pullDown.addEventListener('click', function () {
     subMenu.classList.toggle('sub-menu-open');
 });
 
-/*reset popup*/
-
+//reset popup
 $('.reset-approve').on('click', function () {
     gameReset();
 });
@@ -88,6 +83,7 @@ $('.cancel-button').on('click', function () {
     $(this).closest('.popup-container').fadeOut(0);
 });
 
+//approve popup
 $('#winner-approve').on('click', function () {
     winner.classList.add('display-none');
     gameStats.playerName = $('#playerName').val();
@@ -99,17 +95,14 @@ $('#winner-approve').on('click', function () {
     $('.highscore').fadeIn(0);
 });
 
-/*input player name*/
-$('#player-name-input').on('update', function () {
-    //update gameStats with new value for name
-});
-
-/*game reset*/
+//game reset
 const gameReset = function () {
     // reset the board by randomizing the array
     cardArray.sort(function () {
         return 0.5 - Math.random()
     });
+
+    //place cards
     for (let i = 0; i < 12; i++) {
         const newCard = document.createElement('div');
         newCard.classList.add('card', 'card-' + cardArray[i]);
@@ -119,7 +112,8 @@ const gameReset = function () {
         fragment.appendChild(newCard);
     }
     cardsContainer.innerHTML = '';
-    cardsContainer.append(fragment); // reset timer
+    cardsContainer.append(fragment);
+
     // reset rating, matches and timer
     matches = 0;
     rating.classList.remove('rating-2-3', 'rating-1-3');
@@ -131,70 +125,75 @@ const gameReset = function () {
     gameStats.clicks = 0;
     gameStats.rating = 1;
     cardData = '';
-    cardId = 13;
+    cardId = undefined;
+
     // hide all popups
     $('.full-screen').fadeOut(0);
 }
 
-/*Initial game reset*/
-
+//Initial game reset
 gameReset();
 
-/*card interaction*/
-
+//card interaction
 $(document).on('click', '.card', function () {
+    //start timer
     clock.timer('resume');
-    // toggle .flipped class on card
-    this.classList.toggle("flipped");
-    // add 1 click to every 2 clicks
-    gameStats.clicks++;
-    // determine star rating by evaluating clicks
-    if (gameStats.clicks > 14 && gameStats.clicks < 20) {
-        rating.classList.add('rating-2-3');
-        gameStats.rating = 2;
-    } else if (gameStats.clicks >= 20) {
-        rating.classList.remove('rating-2-3');
-        rating.classList.add('rating-1-3');
-        gameStats.rating = 3;
-    }
-    // evaluate cards
-    if (cardData === $(this).attr('data') && cardId != $(this).attr('id')) {
-        $('*[data=' + cardData + ']').addClass('done');
-        //document.querySelector('*[data=' + cardData + ']').classList.add('done');
-        cardData = '';
-        matches++;
-        cardId = $(this).attr('id');
-        console.log(cardId);
-        console.log(matches);
-        $('#particle-container').fadeIn(0);
-        particleAnimation(150);
-        setTimeout(function () {
-            $('#particle-container').fadeOut(0)
-        }, 1000);
-    } else if (gameStats.clicks % 2 === 0) {
-        setTimeout(function () {
-            $(card).removeClass('flipped');
-        }, 900);
-        cardData = '';
-        cardId = $(this).attr('id');
+
+    //card logic
+    if ($('.flipped').length >= 2) {
+        return
     } else {
-        cardData = $(this).attr('data');
-        cardId = $(this).attr('id');
-    }
-    // evaluate number of succesful matches
-    if (matches === 6) {
-        clock.timer('pause');
-        gameStats.clicks = gameStats.clicks / 2;
-        gameStats.timer = clock.data('seconds');;
-        // update score
-        gameStats.score = Number(Math.round(1000 - gameStats.timer * gameStats.clicks / gameStats.rating));
-        //blow confetti
-        $('#particle-container').fadeIn(0);
-        /*particle animation*/
-        particleAnimation(gameStats.score);
-        // display popup of success
-        setTimeout(function () {
-            winner.classList.remove('display-none');
-        }, 1500);
+        //add 1 to click count
+        gameStats.clicks++;
+        //determine rating
+        if (gameStats.clicks > 14 && gameStats.clicks < 20) {
+            rating.classList.add('rating-2-3');
+            gameStats.rating = 2;
+        } else if (gameStats.clicks >= 20) {
+            rating.classList.remove('rating-2-3');
+            rating.classList.add('rating-1-3');
+            gameStats.rating = 3;
+        }
+        //add flipped class
+        $(this).addClass('flipped');
+        //evaluate cards
+        if (cardData === $(this).attr('data') && cardId != $(this).attr('id')) {
+            $('*[data=' + cardData + ']').addClass('done');
+            //document.querySelector('*[data=' + cardData + ']').classList.add('done');
+            cardData = '';
+            matches++;
+            cardId = $(this).attr('id');
+            $('.card').removeClass('flipped');
+            $('#particle-container').fadeIn(0);
+            particleAnimation(150);
+            setTimeout(function () {
+                $('#particle-container').fadeOut(0)
+            }, 1000);
+        } else if (gameStats.clicks % 2 === 0) {
+            setTimeout(function () {
+                $(card).removeClass('flipped');
+            }, 900);
+            cardData = '';
+            cardId = $(this).attr('id');
+        } else {
+            cardData = $(this).attr('data');
+            cardId = $(this).attr('id');
+        }
+        // evaluate number of succesful matches
+        if (matches === 6) {
+            clock.timer('pause');
+            gameStats.clicks = gameStats.clicks / 2;
+            gameStats.timer = clock.data('seconds');;
+            // update score
+            gameStats.score = Number(Math.round(1000 - gameStats.timer * gameStats.clicks / gameStats.rating));
+            //blow confetti
+            $('#particle-container').fadeIn(0);
+            //particle animation
+            particleAnimation(gameStats.score);
+            // display popup of success
+            setTimeout(function () {
+                winner.classList.remove('display-none');
+            }, 1500);
+        }
     }
 });
